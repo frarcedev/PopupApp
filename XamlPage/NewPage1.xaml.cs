fcc7b4dbef5293541303ad1021ba3588b60d7fc1
frarcedev <64759374+frarcedev@users.noreply.github.com>
2023-08-02 23:49:57 -0400

    MessagingCenter

---
+++ XamlPage/NewPage1.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace PopupApp.XamlPage;
+
+public partial class NewPage1 : ContentPage
+{
+	public NewPage1()
+	{
+		InitializeComponent();
+	}
+
+    public object pruebita;
+
+    void Button_Clicked(System.Object sender, System.EventArgs e)
+    {
+        pruebita = "funciono";
+        MessagingCenter.Send<NewPage1>(this, "WORK");
+        Navigation.PopModalAsync();
+
+    }
+
+}
